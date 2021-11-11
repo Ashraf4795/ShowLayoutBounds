edb02a86ce97762527c01023fbebbe26ea0b1a4c
GitHub Action <action@github.com>
2021-11-11 11:07:36 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/ashraf4795/showlayoutbounds/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.ashraf4795.showlayoutbounds.services
+
+import com.github.ashraf4795.showlayoutbounds.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/ashraf4795/showlayoutbounds/services/MyProjectService.kt
+++ src/main/kotlin/com/github/ashraf4795/showlayoutbounds/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.ashraf4795.showlayoutbounds.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.ashraf4795.showlayoutbounds.MyBundle
 
 class MyProjectService(project: Project) {
 
